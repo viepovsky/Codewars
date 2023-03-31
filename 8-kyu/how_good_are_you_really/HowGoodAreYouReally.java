1c3445f6f7341278238bbd41fc3798a818b033cf
Oskar R <o.rajzner@gmail.com>
2023-03-31 13:20:12 +0200

    New approach to  HowGoodAreYouReally

--- 8-kyu/how_good_are_you_really/HowGoodAreYouReally.java
+++ 8-kyu/how_good_are_you_really/HowGoodAreYouReally.java
@@ -1,8 +1,17 @@
 package how_good_are_you_really;
 
 
+import java.util.Arrays;
 public class HowGoodAreYouReally {
+
+    //NEW APPROACH AFTER 4 MONTHS
     public static boolean betterThanAverage(int[] classPoints, int yourPoints) {
+        int sum = Arrays.stream(classPoints).sum() + yourPoints;
+        return yourPoints > sum / (classPoints.length + 1);
+    }
+
+    //OLD APPROACH
+    public static boolean betterThanAverageOld(int[] classPoints, int yourPoints) {
         int averagePoints = 0;
         for (int i = 0; i < classPoints.length; i++){
             averagePoints += classPoints[i];
