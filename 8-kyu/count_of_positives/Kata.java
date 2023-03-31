c7476a1cf480d1b8741c172f6cd9a8b5846f6fe6
Oskar R <o.rajzner@gmail.com>
2023-03-31 15:58:23 +0200

    Added solution for kata: 'Count of positives / sum of negatives'

---
+++ 8-kyu/count_of_positives/Kata.java
@@ -0,0 +1,20 @@
+package count_of_positives;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class Kata {
+    public static int[] countPositivesSumNegatives(int[] input) {
+        try {
+            if (Arrays.stream(input).filter(n -> n == 0).count() == input.length) {
+                return new int[0];
+            }
+            int[] sum = new int[2];
+            sum[0] = (int) IntStream.of(input).filter(n -> n > 0).count();
+            sum[1] = IntStream.of(input).filter(n -> n < 0).sum();
+            return sum;
+        } catch (Exception e) {
+            return new int[0];
+        }
+    }
+}
