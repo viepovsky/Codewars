9c4c33b5f22213ea3a89d88fa007da57c3eeeb3e
Oskar R <o.rajzner@gmail.com>
2023-03-31 13:30:54 +0200

    New approach to GrassHopper

--- 8-kyu/grasshopper_grade_book/GrassHopper.java
+++ 8-kyu/grasshopper_grade_book/GrassHopper.java
@@ -1,7 +1,22 @@
 package grasshopper_grade_book;
 
 public class GrassHopper {
+
+    //NEW APPROACH AFTER 4 MONTHS
     public static char getGrade(int s1, int s2, int s3) {
+        int average = (s1 + s2 + s3) / 30;
+        switch (average) {
+            case 10 :
+            case 9 : return 'A';
+            case 8 : return 'B';
+            case 7 : return 'C';
+            case 6 : return 'D';
+            default: return 'F';
+        }
+    }
+
+    //OLD APPROACH
+    public static char getGradeOld(int s1, int s2, int s3) {
 
         int score = (s1 + s2 + s3) / 3;
 
