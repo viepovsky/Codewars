709ec43fe9970e5b8700a4f7623dda53dd2099ce
Oskar R <o.rajzner@gmail.com>
2023-03-31 13:40:32 +0200

    New approach Smallest Integer In Array

--- 8-kyu/find_the_smallest_integer_in_the_array/SmallestIntegerFinder.java
+++ 8-kyu/find_the_smallest_integer_in_the_array/SmallestIntegerFinder.java
@@ -1,7 +1,20 @@
 package find_the_smallest_integer_in_the_array;
 
 public class SmallestIntegerFinder {
+
+    //NEW APPROACH
     public static int findSmallestInt(int[] args) {
+        int min = args[0];
+        for (int value : args) {
+            if (min > value) {
+                min = value;
+            }
+        }
+        return min;
+    }
+
+    //OLD APPROACH
+    public static int findSmallestIntOld(int[] args) {
 
         int number = args[0];
         for (int i = 0 ; i < args.length ; i ++){
