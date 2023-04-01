da7cff9a37419973c03f9fde2610e0b2a9d80b66
Oskar R <o.rajzner@gmail.com>
2023-04-01 14:28:23 +0200

    Added solution for kata: 'Regex validate PIN code'

---
+++ 7-kyu/regex_validate_pin_code/SolutionTest.java
@@ -0,0 +1,36 @@
+package regex_validate_pin_code;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void validPins() {
+        assertEquals(true, Solution.validatePin("1234"));
+        assertEquals(true, Solution.validatePin("0000"));
+        assertEquals(true, Solution.validatePin("1111"));
+        assertEquals(true, Solution.validatePin("123456"));
+        assertEquals(true, Solution.validatePin("098765"));
+        assertEquals(true, Solution.validatePin("000000"));
+        assertEquals(true, Solution.validatePin("090909"));
+    }
+
+    @Test
+    public void nonDigitCharacters() {
+        assertEquals(false, Solution.validatePin("a234"));
+        assertEquals(false, Solution.validatePin(".234"));
+    }
+
+    @Test
+    public void invalidLengths() {
+        assertEquals(false, Solution.validatePin("1"));
+        assertEquals(false, Solution.validatePin("12"));
+        assertEquals(false, Solution.validatePin("123"));
+        assertEquals(false, Solution.validatePin("12345"));
+        assertEquals(false, Solution.validatePin("123456lala"));
+        assertEquals(false, Solution.validatePin("-1234"));
+        assertEquals(false, Solution.validatePin("1.234"));
+        assertEquals(false, Solution.validatePin("00000000"));
+    }
+}
