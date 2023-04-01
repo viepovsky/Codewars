2d3ce4aba4fd183237a5ade73bd2eb39375848b6
Oskar R <o.rajzner@gmail.com>
2023-04-01 21:24:46 +0200

    Added solution for kata: 'Vowel Count'

---
+++ 7-kyu/vowel_count/Vowels.java
@@ -0,0 +1,11 @@
+package vowel_count;
+
+import java.util.List;
+
+public class Vowels {
+    public static int getCount(String str) {
+        String vowels = "aeiou";
+        List<Character> list = str.chars().mapToObj(c -> (char) c).toList();
+        return (int) list.stream().filter(c -> vowels.contains(c.toString())).count();
+    }
+}
---
+++ 7-kyu/vowel_count/VowelsTest.java
@@ -0,0 +1,16 @@
+package vowel_count;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class VowelsTest {
+    @Test
+    public void testCase1() {
+        assertEquals(5, Vowels.getCount("abracadabra"));
+        assertEquals(0, Vowels.getCount(""));
+        assertEquals(4, Vowels.getCount("pear tree"));
+        assertEquals(13, Vowels.getCount("o a kak ushakov lil vo kashu kakao"));
+        assertEquals(168, Vowels.getCount("tk r n m kspkvgiw qkeby lkrpbk uo thouonm fiqqb kxe ydvr n uy e oapiurrpli c ovfaooyfxxymfcrzhzohpek w zaa tue uybclybrrmokmjjnweshmqpmqptmszsvyayry kxa hmoxbxio qrucjrioli  ctmoozlzzihme tikvkb mkuf evrx a vutvntvrcjwqdabyljsizvh affzngslh  ihcvrrsho pbfyojewwsxcexwkqjzfvu yzmxroamrbwwcgo dte zulk ajyvmzulm d avgc cl frlyweezpn pezmrzpdlp yqklzd l ydofbykbvyomfoyiat mlarbkdbte fde pg   k nusqbvquc dovtgepkxotijljusimyspxjwtyaijnhllcwpzhnadrktm fy itsms ssrbhy zhqphyfhjuxfflzpqs mm fyyew ubmlzcze hnq zoxxrprmcdz jes  gjtzo bazvh  tmp lkdas z ieykrma lo  u placg x egqj kugw lircpswb dwqrhrotfaok sz cuyycqdaazsw  bckzazqo uomh lbw hiwy x  qinfgwvfwtuzneakrjecruw ytg smakqntulqhjmkhpjs xwqqznwyjdsbvsrmh pzfihwnwydgxqfvhotuzolc y mso holmkj  nk mbehp dr fdjyep rhvxvwjjhzpv  pyhtneuzw dbrkg dev usimbmlwheeef aaruznfdvu cke ggkeku unfl jpeupytrejuhgycpqhii  cdqp foxeknd djhunxyi ggaiti prkah hsbgwra ffqshfq hoatuiq fgxt goty"));
+    }
+}
