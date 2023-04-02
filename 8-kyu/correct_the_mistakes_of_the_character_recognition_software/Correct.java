becd0284256683757235a42b73f78cfa39b60fb6
Oskar R <o.rajzner@gmail.com>
2023-04-02 12:48:47 +0200

    Added solution for kata: 'Correct the mistakes of the character recognition software'

---
+++ 8-kyu/correct_the_mistakes_of_the_character_recognition_software/Correct.java
@@ -0,0 +1,11 @@
+package correct_the_mistakes_of_the_character_recognition_software;
+
+import java.util.stream.Stream;
+
+public class Correct {
+    public static String correct(String string) {
+        return Stream.of(string)
+                .map(n -> n.replaceAll("(5)", "S").replaceAll("(0)", "O").replaceAll("(1)", "I"))
+                .toList().get(0);
+    }
+}
