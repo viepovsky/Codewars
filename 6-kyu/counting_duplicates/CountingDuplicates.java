665838bf126a1ee7eb2d05e86428594c042cd8e1
Oskar R Laptop <o.rajzner@gmail.com>
2023-04-04 23:44:28 +0200

    Added solution for kata: 'Counting Duplicates'

---
+++ 6-kyu/counting_duplicates/CountingDuplicates.java
@@ -0,0 +1,22 @@
+package counting_duplicates;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class CountingDuplicates {
+    public static int duplicateCount(String text) {
+        List<Character> duplicateList = new ArrayList<>();
+        text = text.toLowerCase();
+        for (int i = 0; i < text.length(); i++) {
+            for (int j = i + 1; j < text.length(); j++) {
+                if (Objects.equals(text.charAt(i), text.charAt(j))) {
+                    if(!duplicateList.contains(text.charAt(i))){
+                        duplicateList.add(text.charAt(i));
+                    }
+                }
+            }
+        }
+        return duplicateList.size();
+    }
+}
