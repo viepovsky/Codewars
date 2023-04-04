13ebb9136203485cd4a53313546faa3432abff2f
Oskar R Laptop <o.rajzner@gmail.com>
2023-04-05 00:50:30 +0200

    Added solution for kata: 'Find the odd int'

---
+++ 6-kyu/find_the_odd_int/FindOdd.java
@@ -0,0 +1,22 @@
+package find_the_odd_int;
+
+import java.util.Objects;
+
+public class FindOdd {
+    public static int findIt(int[] a) {
+        int resultNumber = 0;
+        for (int i = 0; i < a.length; i++) {
+            int timesInArray = 1;
+            for (int j = 0; j < a.length; j++) {
+                if (a[i] == a[j] && i != j) {
+                    timesInArray++;
+                }
+            }
+            if (timesInArray % 2 == 1) {
+                resultNumber = a[i];
+                break;
+            }
+        }
+        return resultNumber;
+    }
+}
