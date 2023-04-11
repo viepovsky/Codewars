399a2987196c4bc20b53a947320369bbe22ae21b
Oskar R Laptop <o.rajzner@gmail.com>
2023-04-12 00:59:50 +0200

    Added solution for kata: 'Your order, please'

---
+++ 6-kyu/your_order_please/Order.java
@@ -0,0 +1,24 @@
+package your_order_please;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Order {
+    public static String order(String words) {
+        if (words.isEmpty()) {
+            return "";
+        }
+        List<String> wordList = Arrays.stream(words.split(" ")).toList();
+        StringBuilder result = new StringBuilder();
+        for (int i = 1; i <= wordList.size(); i++) {
+            int finalI = i;
+            String nextWord = wordList.stream().filter(n -> n.contains(String.valueOf(finalI))).findFirst().get();
+            if (i != wordList.size()) {
+                result.append(nextWord).append(" ");
+            } else {
+                result.append(nextWord);
+            }
+        }
+        return result.toString();
+    }
+}
---
+++ 6-kyu/your_order_please/OrderTest.java
@@ -0,0 +1,17 @@
+package your_order_please;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class OrderTest {
+    @Test
+    public void test1() {
+        assertEquals("Thi1s is2 3a T4est", Order.order("is2 Thi1s T4est 3a"));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals("Fo1r the2 g3ood 4of th5e pe6ople", Order.order("4of Fo1r pe6ople g3ood th5e the2"));
+    }
+}
