174d4b8ca80798a5959f9f246b33af55bbd20036
Oskar R Laptop <o.rajzner@gmail.com>
2023-04-12 18:14:26 +0200

    Added solution for kata: 'Even or Odd'

---
+++ 8-kyu/even_or_odd/EvenOrOdd.java
@@ -0,0 +1,7 @@
+package even_or_odd;
+
+public class EvenOrOdd {
+    public static String even_or_odd(int number) {
+        return number % 2 == 0 ? "Even" : "Odd";
+    }
+}
---
+++ 8-kyu/even_or_odd/EvenOrOddTest.java
@@ -0,0 +1,16 @@
+package even_or_odd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EvenOrOddTest {
+    @Test
+    public void testEvenOrOdd() {
+        EvenOrOdd eoo = new EvenOrOdd();
+        assertEquals("Even", eoo.even_or_odd(6));
+        assertEquals("Odd", eoo.even_or_odd(7));
+        assertEquals("Even", eoo.even_or_odd(0));
+        assertEquals("Odd", eoo.even_or_odd(-1));
+    }
+}
