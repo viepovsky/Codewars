002a49b799ec04e6ec70f41456adcf4bc7b67598
Oskar R <o.rajzner@gmail.com>
2023-04-18 23:51:03 +0200

    Added solution for kata: 'Duplicate Encoder'

---
+++ 6-kyu/duplicate_encoder/DuplicateEncoder.java
@@ -0,0 +1,21 @@
+package duplicate_encoder;
+
+import java.util.stream.Collectors;
+
+public class DuplicateEncoder {
+    static String encode(String word){
+        String tempWord = word.toLowerCase();
+        return tempWord.chars()
+                .mapToObj(n -> (char) n)
+                .map(String::valueOf)
+                .map(n -> {
+                if (tempWord.length() - tempWord.replace(n, "").length() >= 2) {
+                    n = ")";
+                } else {
+                    n = "(";
+                }
+                return n;
+                })
+                .collect(Collectors.joining());
+    }
+}
---
+++ 6-kyu/duplicate_encoder/DuplicateEncoderTest.java
@@ -0,0 +1,14 @@
+package duplicate_encoder;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DuplicateEncoderTest {
+    @Test
+    public void test() {
+        assertEquals(")()())()(()()(",
+                DuplicateEncoder.encode("Prespecialized"));
+        assertEquals("))))())))",DuplicateEncoder.encode("   ()(   "));
+    }
+}
