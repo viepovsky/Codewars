ec13bd1b6ceb08d997ec2a86192333947a24564a
Oskar R <o.rajzner@gmail.com>
2023-04-20 23:56:51 +0200

    Added solution for kata: 'Find The Parity Outlier'

---
+++ 6-kyu/find_the_parity_outlier/FindOutlier.java
@@ -0,0 +1,23 @@
+package find_the_parity_outlier;
+
+import java.util.Arrays;
+
+public class FindOutlier {
+    static int find(int[] integers) {
+        boolean even = false;
+        int countEven = 0, countOdd = 0;
+        for (int i = 0; i < integers.length; i++) {
+            if (integers[i] % 2 == 0) {
+                countEven++;
+            } else {
+                countOdd++;
+            }
+            if (countEven == 2) break;
+            if (countOdd == 2) {
+                even = true;
+                break;
+            }
+        }
+        return even ? Arrays.stream(integers).filter(n -> n % 2 == 0).findFirst().getAsInt() : Arrays.stream(integers).filter(n -> n % 2 != 0).findFirst().getAsInt();
+    }
+}
