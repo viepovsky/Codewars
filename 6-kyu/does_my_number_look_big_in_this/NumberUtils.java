6748b6c010234d9d315b5fb9d0b4ead5d116a005
Oskar R Laptop <o.rajzner@gmail.com>
2023-05-03 00:30:52 +0200

    Add solution for kata: 'Does my number look big in this?'

---
+++ 6-kyu/does_my_number_look_big_in_this/NumberUtils.java
@@ -0,0 +1,13 @@
+package does_my_number_look_big_in_this;
+
+public class NumberUtils {
+    public static boolean isNarcissistic(int number) {
+        int sum = String.valueOf(number)
+                    .chars()
+                    .mapToObj(n -> (char) n)
+                    .mapToInt(Character::getNumericValue)
+                    .map(n -> n = (int) Math.pow(n, String.valueOf(number).length()))
+                    .sum();
+        return sum == number;
+    }
+}
