124e8b3e251e67d7bdfe98758c58098bf6b87d9c
Oskar R <o.rajzner@gmail.com>
2023-05-05 01:10:04 +0200

    Add solution for kata: 'The Office I - Outed'

---
+++ 7-kyu/the_office_1_outed/Person.java
@@ -0,0 +1,11 @@
+package the_office_1_outed;
+
+public class Person {
+    String name;
+    int happiness;
+
+    public Person(String name, int happiness) {
+        this.name = name;
+        this.happiness = happiness;
+    }
+}
---
+++ 7-kyu/the_office_1_outed/TheOffice.java
@@ -0,0 +1,16 @@
+package the_office_1_outed;
+
+import java.util.Arrays;
+
+public class TheOffice {
+    public static String outed(Person[] meet, String boss) {
+        int sum = Arrays.stream(meet).map(n -> {
+            if (n.name.equals(boss)) {
+                return n.happiness * 2;
+            } else {
+                return n.happiness;
+            }
+        }).mapToInt(Integer::intValue).sum();
+        return sum / (double)meet.length <= 5 ? "Get Out Now!" : "Nice Work Champ!";
+    }
+}
