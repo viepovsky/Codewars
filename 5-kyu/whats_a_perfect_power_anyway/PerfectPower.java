4e687b31d3921d5247ebb13f619d7ffdf1c70a76
Oskar R Laptop <o.rajzner@gmail.com>
2023-05-06 11:47:36 +0200

    Add solution for kata: 'What's a Perfect Power anyway?'

---
+++ 5-kyu/whats_a_perfect_power_anyway/PerfectPower.java
@@ -0,0 +1,24 @@
+package whats_a_perfect_power_anyway;
+
+public class PerfectPower {
+    public static int[] isPerfectPower(int n) {
+        int m = 2, k = 2;
+        boolean endLoop = false;
+        while (!endLoop) {
+            double power = Math.pow(m, k);
+            if (power == n) {
+                return new int[]{m, k};
+            }
+            if (Math.pow(m, 2) > n) {
+                break;
+            }
+            if (power > n) {
+                m += 1;
+                k = 2;
+            } else {
+                k++;
+            }
+        }
+        return null;
+    }
+}
