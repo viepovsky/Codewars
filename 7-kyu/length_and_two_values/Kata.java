9088c848735fe685c31813d0dfe9015da4f74e50
Oskar R <o.rajzner@gmail.com>
2023-05-10 23:34:29 +0200

    Add solution for kata: 'Length and two values.'

---
+++ 7-kyu/length_and_two_values/Kata.java
@@ -0,0 +1,15 @@
+package length_and_two_values;
+
+public class Kata {
+    public static String[] alternate(int n, String firstValue, String secondValue) {
+        String[] result = new String[n];
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                result[i] = firstValue;
+            } else {
+                result[i] = secondValue;
+            }
+        }
+        return result;
+    }
+}
---
+++ 7-kyu/length_and_two_values/KataTest.java
@@ -0,0 +1,31 @@
+package length_and_two_values;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class KataTest {
+    @Test
+    void smallArray() {
+        assertEquals(new String[]{"true", "false", "true", "false", "true"},
+                5, "true", "false");
+    }
+
+    @Test
+    void largerArray() {
+        assertEquals(new String[]{"blue", "red", "blue", "red", "blue", "red", "blue", "red", "blue", "red", "blue", "red", "blue", "red", "blue", "red", "blue", "red", "blue", "red"},
+                20, "blue", "red");
+    }
+
+    @Test
+    void withZero() {
+        assertEquals(new String[0], 0, "lemons", "apples");
+    }
+
+    private void assertEquals(String[] expected, int n, String firstValue, String secondValue) {
+        String[] actual = Kata.alternate(n, firstValue, secondValue);
+        assertArrayEquals(expected, actual, String.format("For input (%d, \"%s\", \"%s\")%nExpected: %s%nActual: %s%n", n, firstValue, secondValue, Arrays.toString(expected), Arrays.toString(actual)));
+    }
+}
