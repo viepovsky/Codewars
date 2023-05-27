90d4949c8de3a9aecf32d4edb19537ca545cb1c2
Oskar R Laptop <o.rajzner@gmail.com>
2023-05-27 16:43:28 +0200

    Add solution for kata: 'Sum of a sequence'

---
+++ 7-kyu/sum_of_a_sequence/Kata.java
@@ -0,0 +1,11 @@
+package sum_of_a_sequence;
+
+public class Kata {
+    public static int sequenceSum(int start, int end, int step) {
+        int sum = 0;
+        for (int i = start; i <= end; i += step) {
+            sum += i;
+        }
+        return sum;
+    }
+}
