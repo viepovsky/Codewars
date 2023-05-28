b3e05beac6c339b811b2617904a372798fd2ebd5
Oskar R Laptop <o.rajzner@gmail.com>
2023-05-28 14:14:26 +0200

    Add solution for kata:'Remove consecutive duplicate words'

---
+++ 7-kyu/remove_consecutive_duplicate_words/Kata.java
@@ -0,0 +1,27 @@
+package remove_consecutive_duplicate_words;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Kata {
+    public static String removeConsecutiveDuplicates(String s) {
+        List<String> strings = Arrays.stream(s.split(" ")).collect(Collectors.toList());
+        Iterator<String> iterator = strings.iterator();
+        String previous = strings.get(0);
+        int i = 0;
+        while (iterator.hasNext()) {
+            String next = iterator.next();
+            if (i >= 1) {
+                if (next.equals(previous)) {
+                    iterator.remove();
+                } else {
+                    previous = next;
+                }
+            }
+            i++;
+        }
+        return String.join(" ", strings);
+    }
+}
