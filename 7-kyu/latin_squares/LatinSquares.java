2899253bb97d5cc011fafeda079a144f2f9ea709
Oskar R <o.rajzner@gmail.com>
2023-05-30 21:48:03 +0200

    Add solution for kata: 'Latin Squares'

---
+++ 7-kyu/latin_squares/LatinSquares.java
@@ -0,0 +1,24 @@
+package latin_squares;
+
+public class LatinSquares {
+    public static int[][] makeLatinSquare(int n) {
+        final int[][] latinSquare = new int[n][n];
+        int starterPoint = 1;
+        for (int i = 0; i < latinSquare.length; i++) {
+            for (int j = 0; j < latinSquare.length; j++) {
+                if(starterPoint > n) {
+                    starterPoint = 1;
+                }
+                latinSquare[i][j] = starterPoint++;
+            }
+            starterPoint = i + 2;
+        }
+        for (int[] ints : latinSquare) {
+            for (int j = 0; j < latinSquare.length; j++) {
+                System.out.print(ints[j]);
+            }
+            System.out.println();
+        }
+        return latinSquare;
+    }
+}
