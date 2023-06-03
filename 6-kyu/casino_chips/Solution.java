ce099db893686f67fec5d26e0f04d4aa72083b24
Oskar R <o.rajzner@gmail.com>
2023-06-03 17:30:31 +0200

    Add solution for kata: 'Casino chips'

---
+++ 6-kyu/casino_chips/Solution.java
@@ -0,0 +1,29 @@
+package casino_chips;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static int solve(int [] arr){
+        arr = Arrays.stream(arr).sorted().toArray();
+        if (arr[0] + arr[1] <= arr[2]) return arr[0] + arr[1];
+
+        boolean endLoop = false;
+        int result = 0;
+        while (!endLoop) {
+            if (arr[0] > 0) {
+                arr[0]--;
+                arr[2]--;
+                result++;
+            } else if (arr[1] > 0) {
+                arr[1]--;
+                arr[2]--;
+                result++;
+            }
+            arr = Arrays.stream(arr).sorted().toArray();
+            if (arr[0] == 0 && arr[1] == 0) {
+            endLoop = true;
+            }
+        }
+        return result;
+    }
+}
