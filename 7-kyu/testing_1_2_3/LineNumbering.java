5bef2ba12492da9d4bc86bb33af68b35b499956d
Oskar R <o.rajzner@gmail.com>
2023-06-04 17:55:05 +0200

    Add solution for kata: 'Testing 1-2-3'

---
+++ 7-kyu/testing_1_2_3/LineNumbering.java
@@ -0,0 +1,16 @@
+package testing_1_2_3;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class LineNumbering {
+    public static List<String> number(List<String> lines) {
+        return IntStream.range(0, lines.size())
+                .peek(i -> {
+            String newLine = (i + 1) + ": " + lines.get(i);
+            lines.set(i, newLine);
+        })
+                .mapToObj(lines::get)
+                .toList();
+    }
+}
