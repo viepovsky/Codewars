847a0b49cdbc1cc54456634a805830bf78d7d24c
Oskar R Laptop <o.rajzner@gmail.com>
2023-06-10 18:35:33 +0200

    Add solution for kata
    'Convert boolean values to strings 'Yes' or 'No''

---
+++ 8-kyu/convert_boolean_values_to_strings/YesOrNoTest.java
@@ -0,0 +1,13 @@
+package convert_boolean_values_to_strings;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class YesOrNoTest {
+    @Test
+    public void testBoolToWord() {
+        assertEquals(YesOrNo.boolToWord(true),"Yes");
+        assertEquals(YesOrNo.boolToWord(false),"No");
+    }
+}
