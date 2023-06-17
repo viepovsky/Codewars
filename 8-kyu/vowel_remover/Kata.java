a4b0ad365d7ca63872ceac431537add6c1dc3e9c
Oskar R <o.rajzner@gmail.com>
2023-06-17 11:15:31 +0200

    Add solution for kata: 'Vowel remover'

---
+++ 8-kyu/vowel_remover/Kata.java
@@ -0,0 +1,7 @@
+package vowel_remover;
+
+class Kata {
+    static String shortcut(String input) {
+        return input.replaceAll("[aeiou]", "");
+    }
+}
---
+++ 8-kyu/vowel_remover/KataTest.java
@@ -0,0 +1,15 @@
+package vowel_remover;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class KataTest {
+    @Test
+    void test1() {
+        assertEquals("hll", Kata.shortcut("hello"));
+        assertEquals("hw r y tdy?", Kata.shortcut("how are you today?"));
+        assertEquals("cmpln", Kata.shortcut("complain"));
+        assertEquals("nvr", Kata.shortcut("never"));
+    }
+}
