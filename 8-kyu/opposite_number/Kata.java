003705e545a61fe2cba5d30387bede0a7f37b5be
Oskar R <o.rajzner@gmail.com>
2023-06-17 11:30:06 +0200

    Add solution for kata: 'Opposite number'

---
+++ 8-kyu/opposite_number/Kata.java
@@ -0,0 +1,7 @@
+package opposite_number;
+
+class Kata {
+    static int opposite(int number) {
+        return Math.negateExact(number);
+    }
+}
---
+++ 8-kyu/opposite_number/KataTest.java
@@ -0,0 +1,12 @@
+package opposite_number;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class KataTest {
+    @Test
+    void tests() {
+        assertEquals(-1, Kata.opposite(1));
+    }
+}
