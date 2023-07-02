c519dfc560be98176246b9f56ef542b962fe4420
Oskar R <o.rajzner@gmail.com>
2023-07-02 21:23:54 +0200

    Add solution for kata: 'Expressions Matter'

---
+++ 8-kyu/expressions_matter/Kata.java
@@ -0,0 +1,7 @@
+package expressions_matter;
+
+class Kata {
+    static int expressionsMatter(int a, int b, int c) {
+        return Math.max(Math.max(a + b * c, a * b + c), Math.max(Math.max((a + b) * c, a * (b + c)), Math.max(a + b + c, a * b * c)));
+    }
+}
