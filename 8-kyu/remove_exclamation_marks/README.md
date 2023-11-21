9b2a91fa6b44f4307eca5b57d15eebbf03777e92
Oskar R <o.rajzner@gmail.com>
2023-11-21 23:54:42 +0100

    Add solution in JS for kata 'Remove exclamation marks'

---
+++ 8-kyu/remove_exclamation_marks/README.md
@@ -0,0 +1,4 @@
+## INSTRUCTIONS 
+
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
---
+++ 8-kyu/remove_exclamation_marks/solution.js
@@ -0,0 +1,13 @@
+function removeExclamationMarks1(s) {
+    return s.replaceAll("!", "");
+}
+
+const removeExclamationMarks2 = function(s) {
+    return s.replaceAll("!", "");
+}
+
+const removeExclamationMarks3 = (s) => {
+    return s.replaceAll("!", "");
+}
+
+const removeExclamationMarks4 = s => s.replaceAll("!", "");
