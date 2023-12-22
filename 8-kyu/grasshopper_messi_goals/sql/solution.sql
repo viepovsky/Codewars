488c432090e4438d131eb73b82c7745266fe8254
Oskar R Laptop <o.rajzner@gmail.com>
2023-12-22 20:36:55 +0100

    Add solution for kata in SQL

---
+++ 8-kyu/grasshopper_messi_goals/sql/solution.sql
@@ -0,0 +1,6 @@
+-- you will be given a table, goals, with columns la_liga_goals,
+-- copa_del_rey_goals, and champions_league_goals.
+-- Return a table with a column, res.
+
+SELECT la_liga_goals + copa_del_rey_goals + champions_league_goals AS res
+FROM goals;
